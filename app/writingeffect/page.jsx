dd38a2e48f96373b6f0be8d415ad19cd8127563c
Arpan Khare <arpankhare17@gmail.com>
2024-11-01 16:31:15 +0530

    Update page.jsx

--- app/writingeffect/page.jsx
+++ app/writingeffect/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 // what happens if I use simple variable instead of useref outside useeffecr
+// is index really required?
 import React, { useEffect, useState, useRef } from "react";
 import styles from "./writingeffect.module.css";
 const TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur et nisl nec mollis. Morbi quis sapien nec sapien auctor luctus id vel orci. Nam.`;
